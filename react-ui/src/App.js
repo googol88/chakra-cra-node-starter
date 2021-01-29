b5a2827581bfa61fcd92c2d9ae80c9b84aa896c7
googol88 <ronaldfleung@gmail.com>
2021-01-29 12:33:37 -0500

    Initial commit

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,4 +1,83 @@
 import React, { useCallback, useEffect, useState } from 'react';
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  Link,
+  VStack,
+  Code,
+  Grid,
+  theme,
+} from '@chakra-ui/react';
+import { ColorModeSwitcher } from './ColorModeSwitcher';
+import { Logo } from './Logo';
+
+function App() {
+  const [message, setMessage] = useState(null);
+  const [isFetching, setIsFetching] = useState(false);
+  const [url, setUrl] = useState('/api');
+  
+  const fetchData = useCallback(() => {
+    fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`status ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(json => {
+        setMessage(json.message);
+        setIsFetching(false);
+      }).catch(e => {
+        setMessage(`API call failed: ${e}`);
+        setIsFetching(false);
+      })
+  }, [url]);
+
+  useEffect(() => {
+    setIsFetching(true);
+    fetchData();
+  }, [fetchData]);
+  
+  return (
+    <ChakraProvider theme={theme}>
+      <Box textAlign="center" fontSize="xl">
+        <Grid minH="100vh" p={3}>
+          <ColorModeSwitcher justifySelf="flex-end" />
+          <VStack spacing={8}>
+            <Logo h="40vmin" pointerEvents="none" />
+            { process.env.NODE_ENV === 'production' ?
+                <Text>
+                  This is a production build from create-react-app.
+                </Text>
+              : <Text>
+                  Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
+                </Text>
+            }
+            <Text>{'« '}
+            {isFetching
+              ? 'Fetching message from API'
+              : message}
+            {' »'}</Text>
+            <Link
+              color="teal.500"
+              href="https://chakra-ui.com"
+              fontSize="2xl"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn Chakra
+            </Link>
+          </VStack>
+        </Grid>
+      </Box>
+    </ChakraProvider>
+  );
+}
+
+export default App;
+/*
+import React, { useCallback, useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -67,3 +146,4 @@ function App() {
 }
 
 export default App;
+*/
